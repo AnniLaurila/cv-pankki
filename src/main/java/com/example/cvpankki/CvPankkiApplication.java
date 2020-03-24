8296b60a36851386bce7ef209ea7c58169fad512
unknown <anninmaili@gmail.com>
2020-03-24 12:33:25 +0200

    git init

---
+++ src/main/java/com/example/cvpankki/CvPankkiApplication.java
@@ -0,0 +1,13 @@
+package com.example.cvpankki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvPankkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvPankkiApplication.class, args);
+	}
+
+}
