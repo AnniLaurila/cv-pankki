a2689156d5d7e74bf6399c5e2d599f0c7ad8850e
unknown <anninmaili@gmail.com>
2020-03-25 19:20:24 +0200

    lisätty repositoryt

---
+++ src/main/java/com/example/cvpankki/domain/HenkiloRepository.java
@@ -0,0 +1,7 @@
+package com.example.cvpankki.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface HenkiloRepository extends CrudRepository<Henkilo, Long> {
+
+}
---
+++ src/main/java/com/example/cvpankki/domain/TeknologiaosaaminenRepository.java
@@ -0,0 +1,7 @@
+package com.example.cvpankki.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TeknologiaosaaminenRepository extends CrudRepository<Teknologiaosaaminen, Long> {
+
+}
