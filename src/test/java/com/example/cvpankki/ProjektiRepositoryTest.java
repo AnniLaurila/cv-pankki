5e2796d4d205fc33b4491d3e24594857273582ec
unknown <anninmaili@gmail.com>
2020-04-06 14:03:50 +0300

    lisätty projektirepositoryn testit

--- src/test/java/com/example/cvpankki/ProjektiRepositoryTest.java
+++ src/test/java/com/example/cvpankki/ProjektiRepositoryTest.java
@@ -1,6 +1,9 @@
 package com.example.cvpankki;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.example.cvpankki.domain.Projekti;
 import com.example.cvpankki.domain.ProjektiRepository;
 
 @ExtendWith(SpringExtension.class)
@@ -15,11 +19,30 @@ import com.example.cvpankki.domain.ProjektiRepository;
 class ProjektiRepositoryTest {
 
 	@Autowired
-	private ProjektiRepository pRepository;
+	private ProjektiRepository repository;
 	
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testCreateNewProjekti() {
+    	Projekti projekti = new Projekti();
+		repository.save(projekti);
+    	assertThat(projekti.getProjektiId()).isNotNull();
+    } 
+    
+    @Test
+    public void testDeleteProjekti() {
+    	Projekti projekti1 = new Projekti();
+		repository.save(projekti1);
+    	repository.delete(projekti1);
+    	Optional<Projekti> projekti2 = repository.findById(projekti1.getProjektiId());
+    	assertTrue(!projekti2.isPresent());
+    } 
+
+    @Test
+    public void testSearchProjekti() {
+    	Projekti projekti1 = new Projekti();
+		repository.save(projekti1);
+    	Optional<Projekti> projekti2 = repository.findById(projekti1.getProjektiId());
+    	assertTrue(projekti2.isPresent());
+    } 
 
 }
--- src/test/java/com/example/cvpankki/UserRepositoryTest.java
+++ src/test/java/com/example/cvpankki/UserRepositoryTest.java
@@ -26,7 +26,6 @@ class UserRepositoryTest {
 		User user1 = new User("testUser", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
 		repository.save(user1);
     	assertThat(user1.getId()).isNotNull();
-    	//repository.delete(user1);
     } 
     
     @Test
@@ -44,7 +43,6 @@ class UserRepositoryTest {
 		repository.save(user1);
     	Optional<User> user2 = repository.findById(user1.getId());
     	assertTrue(user2.isPresent());
-    	//repository.delete(user1);
     } 
 
 }
